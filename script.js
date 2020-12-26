b29521afe2a05dedbe7644c4031a1d69c35c85ab
sagardubey47 <sagardubey47@gmail.com>
2020-12-26 20:36:53 +0530

    add oninput

--- script.js
+++ script.js
@@ -4,16 +4,34 @@ function apiCall(){
     fetch(`https://en.wikipedia.org/w/api.php?&origin=*&format=json&action=opensearch&search=${value}`)
     .then(response => response.json())
     .then(result => {
-
-        let arrayLink = result[3];
+       
+        let arrayTitle = result[1];
+        let arrayLink  = result[3];
         let innerContent = "";
-        arrayLink.forEach(element => {
-            //<div class="card"><div class="container"><li></li></div></div>
-            innerContent+=`<li><a href=${element} target="_blank">${element}</a></li>`
-        
-        });
+        let count = 0; 
+        if(arrayLink != undefined) {
+            if(arrayLink.length === 0) {
+                innerContent+="<li>so sorry .. no results found</li>";
+            }
+
+            arrayLink.forEach(element => {
+                //<div class="card"><div class="container"><li></li></div></div>
+                innerContent+=`<li><a href=${element} target="_blank">${arrayTitle[count++]}</a></li>`
+            
+            });
+        }
 
         document.getElementById("result").innerHTML = innerContent;
     })
 
 }
+
+//setInterval(apiCall, 500);
+
+
+//document.getElementById("searchBox").addEventListener("change", apiCall);
+document.getElementById("searchBox").oninput = ()=>{
+    apiCall();
+}
+// input.addEventListener("change", apiCall);
+
